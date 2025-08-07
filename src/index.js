59ae30825154b274f3ac90c362ea194b95057cbc
Shoki-Yashiki <Shoki_Yashiki@terumo.co.jp>
2025-08-07 16:04:50 +0900

    aaa

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
